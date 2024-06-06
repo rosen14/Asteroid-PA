0dc5de014a7eee63c1571adc9c8d861c4e937ba1
rosen14 <rosendojesusfazzari@gmail.com>
2024-06-06 11:10:44 -0300

    Primer propuesta de definiciones y esquema

--- Asteroids/src/MyLibrary/obj/Debug/net8.0/MyLibrary.AssemblyInfo.fs
+++ Asteroids/src/MyLibrary/obj/Debug/net8.0/MyLibrary.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("MyLibrary")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f9993e765250b0d8ffe68501cd45ab0fb65d939")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c38f970da8894833743f1dfef67d9d48b16b3e8")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("MyLibrary")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("MyLibrary")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
